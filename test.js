4d477fceae9d6ab3c4a172fcf03b7cc9573a22e4
shujiro <shujiro914@outlook.jp>
2022-07-03 14:31:05 +0900

    first commit

---
+++ test.js
@@ -0,0 +1,36 @@
+//1
+var boolean = true;
+var resultText = boolean ? "Aの処理" : "Bの処理";
+console.log(resultText);
+
+
+//2
+var resultNum = 6 > 5 ? true : false;
+console.log(resultNum);
+
+var resultNum = 4 > 5 ? true : false;
+console.log(resultNum);
+
+
+//3
+var resultNum = 4 < 5 ? true : false;
+console.log(resultNum);
+
+var resultNum = 6 < 5 ? true : false;
+console.log(resultNum);
+
+
+//4
+var resultNum = 10 >= 5 ? true : false;
+console.log(resultNum);
+
+var resultNum = 5 >= 10 ? true : false;
+console.log(resultNum);
+
+
+//5
+var resultNum = 10 ? true : false;
+console.log(resultNum);
+
+var resultNum = null ? true : false;
+console.log(resultNum);
